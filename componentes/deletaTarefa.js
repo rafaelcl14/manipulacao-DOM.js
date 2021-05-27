aedaec59133379880b7dc1a61158337d2b032fc1
Rafael Carvalho Rosina <39815593+rafaelcl14@users.noreply.github.com>
2021-05-27 17:04:59 -0400

    Primeiro commit Projeto

---
+++ componentes/deletaTarefa.js
@@ -0,0 +1,19 @@
+
+        const BotaoDeleta = () => {
+            const botaoDeleta = document.createElement('button');
+            botaoDeleta.innerText = 'Deletar';
+            botaoDeleta.addEventListener('click', DeletarTarefa)
+
+            return botaoDeleta;
+        }
+
+        const DeletarTarefa = (evento) => {
+            const botaoDeleta = evento.target;
+
+            const tarefaCompleta = botaoDeleta.parentElement;
+            tarefaCompleta.remove();
+            return botaoDeleta;
+        }
+
+
+        export default BotaoDeleta;
---
+++ main.js
@@ -0,0 +1,33 @@
+import BotaoConclui from './componentes/concluiTarefa.js'
+import BotaoDeleta from './componentes/deletaTarefa.js'
+        // Evento / Elemento que recebe o evento / O que vai acontecer quando o evento disparar
+
+        const criarTarefa = (evento) => {
+            
+            evento.preventDefault();
+            const lista = document.querySelector('[data-list]');
+            const input = document.querySelector('[data-form-input]');
+            const valor = input.value;
+        
+        
+
+            const tarefa = document.createElement('li');
+            tarefa.classList.add('task')
+            const conteudo = `<p class="content">${valor}</p>`;
+            
+            tarefa.innerHTML = conteudo;
+            tarefa.appendChild(BotaoConclui());
+            tarefa.appendChild(BotaoDeleta());
+            lista.appendChild(tarefa);
+            input.value = " ";
+        }
+
+        const novaTarefa = document.querySelector('[data-form-button]');
+
+        novaTarefa.addEventListener('click', criarTarefa );
+
+       
+
+
+
+
